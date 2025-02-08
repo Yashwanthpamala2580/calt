5e538644cf914e9e6b1aef2c2e2693d3000a4314
yashwanth <pamalayashwanth2580@gmail.com>
2025-02-08 07:57:59 +0530

    Added division function to calt.sh

--- calt.sh
+++ calt.sh
@@ -1,10 +1,42 @@
 #!/bin/bash
 
-echo"enter first number"
+# Addition function
+add() {
+    echo "Result: $(($1 + $2))"
+}
+
+# Subtraction function
+subtract() {
+    echo "Result: $(($1 - $2))"
+}
+
+# Multiplication function
+multiply() {
+    echo "Result: $(($1 * $2))"
+}
+
+# Division function
+divide() {
+    if [ $2 -eq 0 ]; then
+        echo "Error: Division by zero is not allowed!"
+    else
+        echo "Result: $(($1 / $2))"
+    fi
+}
+
+# User input
+echo "Enter first number:"
 read num1
-echo"enter second number"
+echo "Enter second number:"
 read num2
+echo "Choose operation: add, subtract, multiply, divide"
+read operation
 
-sum=$((num1 + num2))
+case $operation in
+    add) add $num1 $num2 ;;
+    subtract) subtract $num1 $num2 ;;
+    multiply) multiply $num1 $num2 ;;
+    divide) divide $num1 $num2 ;;
+    *) echo "Invalid operation!" ;;
+esac
 
-echo "Sum: $sum"
